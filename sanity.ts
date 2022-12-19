c8219effafb68504a66f9fc94974c04e12727d8a
luqmanhxc <luqmanhxc@gmail.com>
2022-12-19 21:19:11 +0800

    fix: remove comments

--- sanity.ts
+++ sanity.ts
@@ -1,10 +1,6 @@
 import { createClient } from "next-sanity";
 import createImageUrlBuilder from "@sanity/image-url";
 
-// const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID; // "pv8y60vp"
-// const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET; // "production"
-// const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION; // "2022-11-16"
-
 export const config = {
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
