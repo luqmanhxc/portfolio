be985a1d44a50857a75abfc8ce653e8274ef375e
luqmanhxc <luqmanhxc@gmail.com>
2022-12-23 05:42:47 +0800

    fix: config change for deployment

--- sanity.ts
+++ sanity.ts
@@ -2,7 +2,7 @@ import { createClient } from "next-sanity";
 import createImageUrlBuilder from "@sanity/image-url";
 
 export const config = {
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "",
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
     apiVersion: "2021-10-21", // https://www.sanity.io/docs/api-versioning
     useCdn: process.env.NODE_ENV === "production", // server-side is statically generated, the CDN is only necessary beneficial if queries are called on-demand
